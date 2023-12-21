6171903813681732b7738dc8163276b5bfba4758
VovaDev777 <polvovater@gmail.com>
2023-12-21 01:11:42 +0100

    1

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+
+
+const URL = 'https://pixabay.com/api/';
+const API_KEY = '40768794-c5228623910588c08b9f5e45d';
+const number_of_page = 40;
+let pageCount = 1;
+
+const axios = require('axios');
+
+
+const userList = document.querySelector('.gallery');
+const search_button = document.querySelector('.search-btn');
+const inputValue = document.querySelector('.input-in-from');
+const loadBtn = document.querySelector('.load-more')
+
+  function fetchInfo() {
+    return axios.get(`${URL}?key=${API_KEY}&per_page=${number_of_page}&page=${pageCount}&q=${inputValue.value}`)
+    .then(response => response.data.hits) 
+    .catch(error => console.log(error));
+  }
+
+
+  function renderMarkup(data) {
+    const markup = data.map((image) => {
+        
+        return `<div class="photo-card">
+        <a href="${image.largeImageURL}" class='large-image'><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="300" height="200"/></a>
+      <div class="info">
+        <p class="info-item">
+         <b>Likes</b>
+          <span>${image.likes}</span>
+        </p>
+        <p class="info-item">
+        <b>Views</b>
+          <span>${image.views}</span>
+        </p>
+        <p class="info-item">
+        <b>Comments</b>
+          <span>${image.comments}</span>
+        </p>
+        <p class="info-item">
+        <b>Downloads</b>
+          <span>${image.downloads}</span>
+        </p>
+      </div>
+    </div>`;
+      })
+      .join("");
+    userList.insertAdjacentHTML('beforeend', markup);
+  }
+
+  search_button.addEventListener('click', (evt) => {
+    loadBtn.classList.remove('hidden');
+    userList.innerHTML = '';
+    evt.preventDefault();
+    fetchInfo()
+    .then((info) => renderMarkup(info))
+    .catch((error) => console.log(error))
+  });
+
+  // fetchInfo().then(data => console.log(data));
+
+  loadBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    pageCount += 1;
+    fetchInfo()
+    .then((info) => renderMarkup(info))
+    .catch((error) => console.log(error))
+  });
